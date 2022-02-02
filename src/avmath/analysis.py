d4d2290b53b173645790e8de7488012e5ed7949d
ballandt <89742906+ballandt@users.noreply.github.com>
2022-02-02 17:54:51 +0100

    polynomial implementation

--- src/avmath/analysis.py
+++ src/avmath/analysis.py
@@ -4,10 +4,11 @@ implementing function features."""
 
 __all__ = ["Point", "Function", "Polynomial"]
 
+import copy
 import sys
 from typing import Union
 
-from . import scope as _scope, REAL, sgn, Fraction
+from . import scope as _scope, REAL, sgn, Fraction, ArgumentError
 
 eps = sys.float_info.epsilon
 
@@ -22,6 +23,9 @@ class Point:
     def __iter__(self):
         for e in self._value:
             yield e
+
+    def __repr__(self):
+        return str(tuple(self))
 
     def __getitem__(self, item):
         return self._value[item]
@@ -265,18 +269,29 @@ class Function:
 
 
 class Polynomial:
+    """A real function a_0 x^n + a_1 x^{n-1} + ... + a_{n-1} x + a_n
+    with real n.
+    """
 
     def __init__(self, *args):
+        """Insert arguments in analytical order:
+        args[0] x^n + args[1] x^{n-1} + ...
+        """
         self._value = list(args)
 
     def __iter__(self):
+        """Yields an iterator of the polynomial coefficients."""
         for e in self._value:
             yield e
 
     def __getitem__(self, item):
+        """Allows item access for coefficients."""
         return self._value[item]
 
     def __repr__(self):
+        """Gives string return in form
+        f(x) = (a_0)x^n + (a_1)x^{n-1} + ...
+        """
         ret_str = "f(x) = "
         for i, e in enumerate(self._value):
             power = self.grade()-i
@@ -288,7 +303,41 @@ class Polynomial:
                 ret_str += str(e)
         return ret_str
 
+    def __neg__(self):
+        """Returns negative polynomial. All coefficient signs are switched."""
+        return Polynomial(*tuple([-e for e in self]))
+
+    def __add__(self, other):
+        """Adds two polynomials or a polynomial with a REAL."""
+        if type(other) == Polynomial:
+            self_list, self_len = list(self), len(list(self))
+            other_list, other_len = list(other), len(list(other))
+            if self_len > other_len:
+                other_list = [0 for _ in range(self_len - other_len)]\
+                             + other_list
+            elif self_len < other_len:
+                self_list = [0 for _ in range(other_len - self_len)]\
+                             + other_list
+            arg_list = [e + other_list[i] for i, e in enumerate(self_list)]
+        elif type(other) in (int, float, Fraction):
+            arg_list = list(self)
+            arg_list[-1] += other
+        else:
+            raise ArgumentError(type(other), "function or real number")
+        return Polynomial(*tuple(arg_list))
+
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        """Subtracts polynomials from another or REAL from polynomial."""
+        return self + -other
+
+    def __rsub__(self, other):
+        """Reversed subtraction."""
+        return -self + other
+
     def __mul__(self, other):
+        """Multiplies two polynomials or a polynomial with a REAL."""
         arg_list = []
         if type(other) == Polynomial:
             arg_list = [0 for _ in range(self.grade() + other.grade()+1)]
@@ -301,18 +350,58 @@ class Polynomial:
             arg_list = [other * e for e in self._value]
         return Polynomial(*tuple(arg_list))
 
+    __rmul__ = __mul__
+
     def at(self, x):
+        """Returns y value to given x"""
         res = 0
         for i, e in enumerate(self._value):
             res += e * x**(self.grade()-i)
         return res
 
     def grade(self):
+        """Returns the grade of the polynomial."""
         return len(self._value) - 1
 
-    def root(self):
+    def newton_method(self, x_n, max_steps: int = 10000):
+        """Executes Newton's method to find a root of the polynomial
+        numerically. Used for root-method."""
+        for _ in range(max_steps):
+            if self.derivative(x_n) != 0:
+                mem_x_n = x_n
+                x_n = x_n - self.at(x_n) / self.derivative(x_n)
+                if abs(x_n - mem_x_n) < 1e-16:
+                    break
+            else:
+                x_n += 1e-2
+        if abs(self.at(x_n)) < 1e-12:
+            return x_n
+        else:
+            raise ArithmeticError("No real root found")
+
+    def horner(self, x_0: int | float = 0):
+        """Returns a polynomial with executed Horner's scheme with given
+        root x_0. Note that the root must be exact for proper results.
+        """
+        arg_list = []
+        product = 0
+        for i, e in enumerate(self._value[:-1]):
+            arg_list.append(e + product)
+            product = x_0 * (e + product)
+        return Polynomial(*tuple(arg_list))
+
+    def roots(self):
+        """Returns a list of the real and complex roots of the polynomial if
+        the number of real roots is
+        n_roots >= grade - 2
+        Else the Horner's scheme fails. The real_roots method returns all
+        real roots of a polynomial without causing errors."""
         if self.grade() == 0:
-            return []
+            if self[0] != 0:
+                return []
+            else:
+                return ArithmeticError("Null function has an infinite amount"
+                                       "of roots")
         elif self.grade() == 1:
             return [Fraction(-self[1], self[0])]
         elif self.grade() == 2:
@@ -321,25 +410,92 @@ class Polynomial:
                 (-self[1]+(self[1]**2-4*self[0]*self[2])**.5)/(2*self[0])
             ]
             return values
-
-    def derivative(self, x: float | int = None):
-        arg_list = []
-        for i, e in enumerate(self._value[:-1]):
-            arg_list.append(e * (self.grade() - i))
-        derivative = Polynomial(*tuple(arg_list))
-        if x:
-            return derivative.at(x)
         else:
-            return derivative
+            try:
+                return [self.newton_method(0)]\
+                       + self.horner(self.newton_method(0)).roots()
+            except ArithmeticError:
+                raise ArithmeticError("Horner's scheme failed. Use real_roots"
+                                      "method for all found results.")
 
-    def integral(self, a: REAL = None, b: REAL = None):
-        arg_list = []
-        for i, e in enumerate(self._value):
-            power = self.grade() - i + 1
-            arg_list.append(Fraction(e, power))
-        arg_list.append(0)
-        integral = Polynomial(*tuple(arg_list))
+    def real_roots(self):
+        """Returns all real roots of a polynomial. Should be used if the
+        polynom has possibly more than grade - 2 complex roots because the
+        default roots method crashes.
+        """
+        if self.grade() == 1:
+            return self.roots()
+        elif self.grade() == 2:
+            return self.roots() if type(self.roots()[0]) != complex else []
+        else:
+            ret_list = []
+            function = self
+            while len(ret_list) < self.grade():
+                try:
+                    newton_root = function.newton_method(0)
+                    ret_list.append(newton_root)
+                except ArithmeticError:
+                    return ret_list
+                try:
+                    horner_roots = function.horner(newton_root).roots()
+                    for e in horner_roots:
+                        if type(e) != complex:
+                            horner_roots.append(e)
+                    if len(horner_roots) == self.grade() - 1:
+                        return ret_list
+                except ArithmeticError:
+                    return ret_list
+            return ret_list
+
+    def max(self):
+        """Returns the maxima of the polynomial based on the real roots."""
+        x_values = self.derivative().real_roots()
+        ret_list = []
+        for e in x_values:
+            if self.derivative(e, grade=2) < 0:
+                ret_list.append(Point(e, self.at(e)))
+        return ret_list
+
+    def min(self):
+        """Returns the minima of a function based on the real roots."""
+        return [e.negative_y() for e in (-self).max()]
+
+    def derivative(self,
+                   x: float | int = None,
+                   grade: int = 1) -> REAL | 'Polynomial':
+        """Returns the derivative polynom if no x is specified. Else returns
+        the value of the derivative at given x.
+        """
+        function = copy.deepcopy(self)
+        for _ in range(grade):
+            arg_list = []
+            if len(function._value) == 1:
+                arg_list = [0]
+            for i, e in enumerate(function._value[:-1]):
+                arg_list.append(e * (function.grade() - i))
+            function = Polynomial(*tuple(arg_list))
+            if function._value == [0]:
+                break
+        if x is not None:
+            return function.at(x)
+        else:
+            return function
+
+    def integral(self,
+                 a: REAL = None,
+                 b: REAL = None,
+                 grade: int = 1) -> REAL | 'Polynomial':
+        """Returns the integral polynom if no a and b specified. Else returns
+        the integral between a and b."""
+        function = copy.deepcopy(self)
+        for _ in range(grade):
+            arg_list = []
+            for i, e in enumerate(function._value):
+                power = function.grade() - i + 1
+                arg_list.append(Fraction(e, power))
+            arg_list.append(0)
+            function = Polynomial(*tuple(arg_list))
         if not a and b:
-            return integral
+            return function
         elif a and b:
-            return integral.at(b) - integral.at(a)
+            return function.at(b) - function.at(a)
