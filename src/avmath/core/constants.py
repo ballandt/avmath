05a579944a9dedcd1698a7a1471e9464f0ed621c
ballandt <89742906+ballandt@users.noreply.github.com>
2022-03-08 23:00:48 +0100

    constants new format

---
+++ src/avmath/core/constants.py
@@ -0,0 +1,26 @@
+"""AVMATH CONSTANTS
+
+Definition of the constants and important number sets of avmath.
+"""
+
+# Euler's number
+e = 2.718_281_828_459_045_235_360
+
+# Pi
+pi = 3.141_592_653_589_793_238_463
+
+# Tau (= 2*pi)
+tau = 6.283_185_307_179_586_476_925
+
+# Golden ration phi
+phi = 1.618_033_988_749_894_848_205
+
+# Euler-Mascheroni constant gamma
+gamma = 0.577_215_664_901_532_860_607
+
+# Number sets
+two_digit_primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
+                    59, 61, 67, 71, 73, 79, 83, 89, 97}
+
+square_numbers_to_20 = {0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169,
+                        196, 225, 256, 289, 324, 361, 400}
